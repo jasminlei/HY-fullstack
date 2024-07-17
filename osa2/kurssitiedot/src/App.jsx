a949c2f7f81c95dfc1c9704873dd2e32d1f30fce
jasminlei <jasminleinonen@hotmail.com>
2024-07-17 19:19:07 +0300

    Tehtävä 2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,26 +1,5 @@
 import Course from './components/Course'
 
-
-const Header = ({ courseName }) => {
-  return (
-    <h1>{courseName}</h1>
-  )
-}
-
-const Content = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <div>
-      {parts.map(part => (
-        <div key={part.id}><p>{part.name} {part.exercises}</p></div>
-      ))}
-      <p><strong>total of {totalExercises} exercises</strong></p>
-
-
-    </div>
-  )
-}
-
 const App = () => {
   const courses = [
     {
